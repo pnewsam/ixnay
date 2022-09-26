0ec40e954d3dd7dc76706409f22b5f869adefe2f
pnewsam <paul.newsam@gmail.com>
2022-09-26 08:17:29 -0700

    Dialog, unused elements.

--- src/css/index.ts
+++ src/css/index.ts
@@ -1,7 +1,10 @@
 import "./root.css";
 
 import "./button.css";
+import "./blockquote.css";
+import "./code.css";
 import "./input.css";
+import "./dialog.css";
 import "./select.css";
 import "./table.css";
 import "./typography.css";
